4ef03b47ad5f5b5158f0dba6ab97fdcb42ae4342
betxcircle <150256421+betxcircle@users.noreply.github.com>
2025-05-13 18:35:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -500,25 +500,31 @@ function generateRoomId() {
 const checkWin = (board) => {
   const winningLines = [
     // Rows
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [9, 10, 11],
+    [12, 13, 14],
 
     // Columns
-    [0, 4, 8, 12],
-    [1, 5, 9, 13],
-    [2, 6, 10, 14],
-    [3, 7, 11, 15],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [4, 7, 10],
+    [5, 8, 11],
+    [6, 9, 12],
+    [7, 10, 13],
+    [8, 11, 14],
 
     // Diagonals
-    [0, 5, 10, 15],
-    [3, 6, 9, 12],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
 
   for (let line of winningLines) {
-    const [a, b, c, d] = line;
-    if (board[a] && board[a] === board[b] && board[a] === board[c] && board[a] === board[d]) {
+    const [a, b, c] = line;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
       return board[a];
     }
   }
